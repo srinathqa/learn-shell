bd75e1b54085dadde02cc74afafe94a71d7b8039
Srinath Bathina <Srinath.Bathina@infor.com>
2024-04-21 09:59:59 +0530

    fixed commented line

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,6 +4,6 @@ echo "Please enter user name: "
 read USERNAME # take user name as input
 
 echo "PLease enter passowrd: "
-read PASSWORD "take user passowrd
+read PASSWORD #take user passowrd
 
 echo "Entered user is $USERNAME and Password is $PASSWORD"
