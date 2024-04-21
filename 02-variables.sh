f29b49ab917b982e98f27ef9a55042c46a5221fd
Srinath Bathina <Srinath.Bathina@infor.com>
2024-04-21 09:50:33 +0530

    To check variables with two parametes passing command line arguments

---
+++ 02-variables.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+#declaring variables
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 :: Hello, Mr/Mrs. $PERSON2, How are you?"
+echo "$PERSON2 :: Hi, Mr/Mrs. $PERSON1, I am fine. Thank you. How are you?"
+echo "$PERSON1 :: I am fine too. How is your job?"
+echo "$PERSON2 :: Not bad, but thinking to upgrade to DevOps!"
