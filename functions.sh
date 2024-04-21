ff269b5154db54bd50d458af543ec5e85170e543
Srinath Bathina <Srinath.Bathina@infor.com>
2024-04-21 18:56:47 +0530

    updated syntax error for color code and path file

--- functions.sh
+++ functions.sh
@@ -1,9 +1,9 @@
 #!bin/bash
 
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-LOG_FILE=/temp/$SCRIPT_NAME-$TIMESTAMP.log
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
@@ -13,7 +13,7 @@ VALIDATE(){
     then
         echo -e "$2.... $R FAILIRE $N"
     else
-        echo "$2.....$G SUCCESS $N"
+        echo -e "$2.....$G SUCCESS $N"
     fi
 }
 
@@ -25,10 +25,10 @@ else
     echo "You are good to go for instllation...."
 fi
 
-dnf install mysql -y &>>$LOG_FILE
+dnf install mysql -y &>>$LOGFILE
 VALIDATE $? "Installing MYSQL...."
 
-dnf install git -y &>>$LOG_FILE
+dnf install git -y &>>$LOGFILE
 VALIDATE $? "Installing GIT ...."
 
 
