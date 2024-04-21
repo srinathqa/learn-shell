d33594153c7e00da3dac555c802b210212900e69
Srinath Bathina <Srinath.Bathina@infor.com>
2024-04-21 11:22:30 +0530

    fixed code to cal values correctly

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -7,7 +7,7 @@ num2=$2
 #echo "Please enter number2 : "
 #read num2 
 
-sum=$($num1+$num2)
-product=$($num1*$num2)
+sum=$(($num1+$num2))
+product=$(($num1*$num2))
 echo "Sum of two numbers: $sum"
 echo "Product of two numbers: $product"
