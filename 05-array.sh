1fab74c01791bbd09420cf82580ddb7ab459c750
Srinath Bathina <Srinath.Bathina@infor.com>
2024-04-21 10:29:14 +0530

    fixed code to print array value

--- 05-array.sh
+++ 05-array.sh
@@ -4,8 +4,8 @@
 Movies=("RRR","DJ","Harry Poter","AVENGERS","Jurasic Park")
 
 #Print array
-echo "First Movie in the array is ($Movies[0])"
-echo "Second Movie in the array is ($Movies[1)"
-echo "Third Movie in the array is ($Movies[2)"
-echo "Fourth Movie in the array is ($Movies[3])"
-echo "Fifth Movie in the array is ($Movies[4])"
+echo "First Movie in the array is  ${Movies[0]}"
+echo "Second Movie in the array is ${Movies[1]}"
+echo "Third Movie in the array is  ${Movies[2]}"
+echo "Fourth Movie in the array is ${Movies[3]}"
+echo "Fifth Movie in the array is  ${Movies[4]}"
