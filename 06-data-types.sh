2bb69300e5bb5b90a9d44f76953a53162017bb4d
Srinath Bathina <Srinath.Bathina@infor.com>
2024-04-21 11:20:08 +0530

    upd code to pass values through arguments

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -1,12 +1,13 @@
 #!bin/bash
 num1=$1
 num2=$2
-echo "Please enter number1 : "
-read num1
-echo "Please enter number2 : "
-read num2
 
-sum=$num1+$num2
-product=$num1*$num2
+#echo "Please enter number1 : "
+#read num1
+#echo "Please enter number2 : "
+#read num2 
+
+sum=$($num1+$num2)
+product=$($num1*$num2)
 echo "Sum of two numbers: $sum"
 echo "Product of two numbers: $product"
