30edf45bd088aa152cae1699988cdebed4adcdae
Srinath Bathina <Srinath.Bathina@infor.com>
2024-04-21 10:24:16 +0530

    test array code

---
+++ 05-array.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+#Declare array
+Movies=("RRR","DJ","Harry Poter","AVENGERS","Jurasic Park")
+
+#Print array
+echo "First Movie in the array is ($Movies[0])"
+echo "Second Movie in the array is ($Movies[1)"
+echo "Third Movie in the array is ($Movies[2)"
+echo "Fourth Movie in the array is ($Movies[3])"
+echo "Fifth Movie in the array is ($Movies[4])"
