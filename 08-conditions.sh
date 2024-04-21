23801b865b2fcca6c6bf3a2bf25114fe8b1d9840
Srinath Bathina <Srinath.Bathina@infor.com>
2024-04-21 12:02:54 +0530

    fixed condition syntax

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -6,7 +6,7 @@ number2=$2
 echo "Entered Number of arguements passed: $@"
 echo "No of arguments passed $#"
 
-if [ $number1 -gt $number2]
+if [ $number1 -gt $number2 ]
 then
     echo "number1 is greather than number2 "
 else
