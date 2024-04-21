e2db59552a9442a52a49f7d00486ec23aa043000
Srinath Bathina <Srinath.Bathina@infor.com>
2024-04-21 09:57:33 +0530

    Taking input from commad line

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+echo "Please enter user name: "
+read USERNAME # take user name as input
+
+echo "PLease enter passowrd: "
+read PASSWORD "take user passowrd
+
+echo "Entered user is $USERNAME and Password is $PASSWORD"
