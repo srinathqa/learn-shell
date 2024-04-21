df9103fb0eee26fec376d7697bd266482054ee90
Srinath Bathina <Srinath.Bathina@infor.com>
2024-04-21 11:58:40 +0530

    check conditonal commands in shell

---
+++ 08-conditions.sh
@@ -0,0 +1,18 @@
+#!bin/bash
+
+number1=$1
+number2=$2
+
+echo "Enter numbers are: $@"
+echo "No of arguments passed $#"
+
+if [ $number1 -gt $number2]
+then
+    echo "number1 is greather than number2"
+else
+    echo "number1 is less than number2"
+fi
+
+echo "conditional commands are -gt, -ge, -lt, -le, -eq
+
+
